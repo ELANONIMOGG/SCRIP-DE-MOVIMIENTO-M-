a4c52b49fef9a74132c60d189f6c30e0ce5eae5d
ELANONIMOGG <94863947+ELANONIMOGG@users.noreply.github.com>
2022-06-14 20:44:30 -0500

    Add files via upload

---
+++ Script movimiento.py
@@ -0,0 +1,11 @@
+from turtle import position
+import keyboard
+import pyautogui as pa
+ 
+
+for i in range(10):
+    pa.moveTo(x=266, y=464), pa.moveTo(x=192, y=210)
+    pa.moveTo(x=800, y=800), pa.moveTo(x=910, y=312)
+    pa.moveTo(x=190, y=510), pa.moveTo(x=312, y=432)
+    pa.position()
+    
